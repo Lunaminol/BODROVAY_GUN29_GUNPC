60393d91b8ac95c61742561316cfa4a2ca662892
Lunaminol <156945356+Lunaminol@users.noreply.github.com>
2024-04-18 02:36:26 +0300

    Add files via upload

---
+++ HW1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿if (int.TryParse(Console.ReadLine(), out int a))
+{
+
+}
+else
+{
+    Console.WriteLine("Error!");
+    return;
+}
+
+if (int.TryParse(Console.ReadLine(), out int b))
+{
+
+}
+else
+{
+    Console.WriteLine("Error!");
+    return;
+}
+
+char s;
+s = Console.ReadKey().KeyChar;
+if (s is not '&' and not '^' and not '|')
+{
+    Console.WriteLine("Error!");
+    return;
+}
+
+switch (s)
+{
+    case '&':
+        Console.WriteLine(a & b);
+        Console.WriteLine(Convert.ToString(a & b, 2));
+        Console.WriteLine(Convert.ToString(a & b, 16));
+        break;
+    case '^':
+        Console.WriteLine(a ^ b);
+        int w = (a ^ b);
+        Console.WriteLine(Convert.ToString(a ^ b, 2));
+        Console.WriteLine(Convert.ToString(a ^ b, 16));
+        break;
+    case '|':
+        Console.WriteLine(a | b);
+        Console.WriteLine(Convert.ToString(a | b, 2));
+        Console.WriteLine(Convert.ToString(a | b, 16));
+        break;
+    default:
+        Console.WriteLine("Wrong sign!");
+        break;
+}
