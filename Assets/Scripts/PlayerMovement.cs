b708d7d412ea9fbf3ac38904b20b642be5c6f892
Yulia Bodrova <yulibodrova@yandex.ru>
2025-02-11 00:57:11 +0300

    Tilemap Homework

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _player;
+    private Camera _mainCamera;
+    private Tilemap _map;
+
+    private void Start()
+    {
+        _map = GetComponent<Tilemap>();
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 clickWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int clickCellPosition = _map.WorldToCell(clickWorldPosition);
+
+            _player.transform.position = clickCellPosition;
+        }
+    }
+}
